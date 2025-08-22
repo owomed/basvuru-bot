4eb05cd1d704a252a09ca66ea62eba418caae1ba
owomed <owomedsunucu@gmail.com>
2025-08-22 18:09:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ client.once('ready', async () => {
             name: 'MED 🤎 OwO ile ilgileniyor',
             type: ActivityType.Custom
         }],
-        status: 'online'
+        status: 'dnd'
     });
 });
 
